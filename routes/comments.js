9d5db83099815997c03870eec61773e421009f07
Jordi <jordi@iceverg.com>
2021-03-06 11:43:09 +0100

    added create route

--- routes/comments.js
+++ routes/comments.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const listController = require('../controllers/comments/list')
+
 /* GET comments listing. */
 router.get('/', listController.listComments);
+router.get('/create', function (req, res) {
+  res.send('you are creating a comment')
+});
 
 module.exports = router;
