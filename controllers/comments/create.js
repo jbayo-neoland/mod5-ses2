57136a7dc43c07cde7e200d96627a95aae77ea7c
Jordi <jordi@iceverg.com>
2021-03-06 13:43:42 +0100

    Added new route comments/:id

--- controllers/comments/create.js
+++ controllers/comments/create.js
@@ -7,7 +7,9 @@ module.exports = {
     res.render('comments/create')
   },
   createCommentPost: async function (req, res) {
+    // 1, 2
     let created = await commentsModel.create(req.body);
+    // 3
     if (!created) {
       res.send('There was an error');
     }
--- routes/comments.js
+++ routes/comments.js
@@ -8,6 +8,11 @@ router.get('/', listController.listComments);
 // GET create comment => renders the view
 router.get('/create', createController.createCommentGet);
 
+/* GET comments listing. */
+router.get('/:id', function(req, res, next) {
+  const id = req.params.id;
+  res.send(JSON.stringify(req.params));
+});
 
 // POST create comment => processes the data of a form
 router.post('/create', createController.createCommentPost);
