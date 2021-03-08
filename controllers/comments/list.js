b56675bb57a2fbe11400c6c12a8c7a6f19acb500
Jordi <jordi@iceverg.com>
2021-03-08 20:55:38 +0100

    Using query params

--- controllers/comments/list.js
+++ controllers/comments/list.js
@@ -3,8 +3,15 @@ const commentsModel = require('../../models/comments');
 
 module.exports = {
   listComments: async function(req, res, next) {
+    let {page, limit} = req.query;
+    if (!limit) {
+      limit = 10;
+    }
+    if (!page) {
+      page = 1;
+    }
     // 1.2
-    let comments = await commentsModel.find();
+    let comments = await commentsModel.find({"_limit": limit, "_page": page});
     // 2
     res.render('comments/list', {title:"List of comments", listComments: comments });
   }
