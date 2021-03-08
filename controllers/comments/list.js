4200bcb956d4b92709d96899c0abe0648a9bc41d
Jordi <jordi@iceverg.com>
2021-03-08 21:44:32 +0100

    added pagination

--- controllers/comments/list.js
+++ controllers/comments/list.js
@@ -4,15 +4,20 @@ const commentsModel = require('../../models/comments');
 module.exports = {
   listComments: async function(req, res, next) {
     let {page, limit} = req.query;
-    if (!limit) {
-      limit = 10;
-    }
-    if (!page) {
-      page = 1;
-    }
+    limit = limit ? parseInt(limit) : 10;
+    page = page ? parseInt(page) : 1;
+
+    let nextPage = page + 1;
+    let prevPage = page - 1 > 0 ? page - 1 : 0;
     // 1.2
     let comments = await commentsModel.find({"_limit": limit, "_page": page});
     // 2
-    res.render('comments/list', {title:"List of comments", listComments: comments });
+    res.render('comments/list', {
+      title:"List of comments",
+      listComments: comments,
+      prevPage: prevPage,
+      nextPage: nextPage,
+      limit: limit
+    });
   }
 }
