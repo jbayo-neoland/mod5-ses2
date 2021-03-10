07f31980c163e259d5f01ecb41290ba296b913f6
Jordi <jordi@iceverg.com>
2021-03-10 19:52:55 +0100

    routing with multiple callbacks / controllers

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,16 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get(/.*fly$/, function(req, res, next) {
+  console.log(`callback 1`, req.app.locals.patata);
+  req.app.locals.patata = 'frita';
+  next();
+}, function(req, res, next) {
+  console.log(`callback 2`,req.app.locals.patata);
+  next();
+}, function(req, res, next) {
+  console.log(`callback 3`,req.app.locals.patata);
+  res.send(JSON.stringify(req.params));
+});
 
 module.exports = router;
